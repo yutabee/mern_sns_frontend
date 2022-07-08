acf21fa761d45e6a22f9bc9beacb7289bb1c0695
yutabee <bsk87m@gmail.com>
2022-07-08 14:29:03 +0900

    rightbar styled

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -4,7 +4,32 @@ import './Rightbar.css'
 export const Rightbar = () => {
   return (
     <>
-         <div className="rightbar">Rightbar</div>
+      <div className="rightbar">
+        <div className="rightbarWrapper">
+          <div className="eventContainer">
+            <img src="assets/star.png" alt="" className='starImg' />
+            <span className='eventText'><b>フォロワー限定</b>イベント開催中！</span>
+          </div>
+          <img src="assets/event.jpeg" alt="" className='eventImg'/>
+          <h4 className="rightbarTitle">オンラインの友達</h4>
+          <ul className='rightbarFriendList'>
+            <li className="rightbarFriend">
+                <div className="rightbarProfileImgContainer">
+                  <img src="assets/person/1.jpeg" alt="" className='rightbarProfileImg' />
+                  <span className="rightbarOnline"></span>
+                </div>
+                <span className="rightbarUsername">Yuuuuta</span>
+            </li>
+             <li className="rightbarFriend">
+                <div className="rightbarProfileImgContainer">
+                  <img src="assets/person/2.jpeg" alt="" className='rightbarProfileImg' />
+                  <span className="rightbarOnline"></span>
+                </div>
+                <span className="rightbarUsername">tanaka</span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
