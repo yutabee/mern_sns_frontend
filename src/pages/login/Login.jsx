4d2f0e349ab6fd8e788a958979ed98264a76c0cc
yutabee <bsk87m@gmail.com>
2022-07-10 22:57:10 +0900

    useRefでinputのvalueを取得

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react'; 
 import './Login.css';
 
 export const Login = () => {
+  const email = useRef();
+  const password = useRef();   //useRefでinputの値を監視できる(簡単に値を取得できるから便利)
+  // console.log(email);
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //ファームボタンを押した時のリロードを防止する
+    // console.log(email.current);
+    console.log(email.current.value);
+    console.log(password.current.value);
+
+}
+
   return (
     <>
         <div className="login">
@@ -10,16 +22,18 @@ export const Login = () => {
                         <h3 className='loginLogo'>MERN SNS</h3>
                         <span className='loginDesc'>本格的なSNSを自分の手で。</span>
                     </div>
-                  <div className="loginRight">
-                    <div className="loginBox">
-                          <p className='loginMsg'>ログインはこちら</p>    
-                          <input type="text" className='loginInput' placeholder='Eメール' />
-                          <input type="text" className='loginInput' placeholder='バスワード' />
-                          <button className='loginButton'>ログイン</button>
-                          <span className='loginForgot'>パスワードを忘れた方へ</span>
-                          <button className='loginRegisterButton'>アカウント作成</button>
-                    </div>
-                  </div>
+                  
+                      <div className="loginRight">
+            <form className="loginBox" onSubmit={(e)=>handleSubmit(e)}>
+                              <p className='loginMsg'>ログインはこちら</p>    
+                              <input type="email" className='loginInput' placeholder='Eメール' required ref={email}/>
+              <input type="password" className='loginInput' placeholder='バスワード' required minLength="6" ref={password} />
+                              <button className='loginButton'>ログイン</button>
+                              <span className='loginForgot'>パスワードを忘れた方へ</span>
+                              <button className='loginRegisterButton'>アカウント作成</button>
+                        </form>
+                      </div>
+          
               </div>    
       </div>
     </>
