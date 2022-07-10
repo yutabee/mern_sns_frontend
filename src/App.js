277f95aeff7644da15504894cde8cb3a93d589d6
yutabee <bsk87m@gmail.com>
2022-07-10 12:36:53 +0900

    routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import { Home } from "./pages/home/Home";
-// import { Profile } from "./pages/profile/Profile";
-// import { Login } from "./pages/login/Login";
-// import { Register } from "./pages/register/Register";
-
+import { Profile } from "./pages/profile/Profile";
+import { Login } from "./pages/login/Login";
+import { Register } from "./pages/register/Register";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Home /> 
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/profile/:username' element={<Profile />} />
+        </Routes>  
+      </Router>  
       {/* <Profile /> */}
       {/* <Login/> */}
       {/* <Register/> */}
